9c3e7600616829f46e09718fb7e8c4aae1a5224d
salaheldinsoliman <salaheldin_sameh@aucegypt.edu>
2024-11-27 12:22:29 +0100

    feat(backend): try to mint some coins for a recieved address
    Signed-off-by: salaheldinsoliman <salaheldin_sameh@aucegypt.edu>

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,44 @@
 use axum::{
-    routing::get,
+    extract::Json,
+    routing::{get, post},
     Router,
 };
 
+mod utils;
 
+
+use iota_sdk::types::base_types::IotaAddress;
 use iota_sdk::IotaClientBuilder;
+use serde::Deserialize;
+
+use iota_sdk::types::transaction::{self, TransactionDataV1};
+
+
+
+
+#[derive(Debug, Deserialize)]
+pub struct GaslessTransactionRequest {
+    pub sender: IotaAddress,
+}
+
+
+async fn handle_gasless_transaction(Json(payload): Json<GaslessTransactionRequest>) {
+    println!("Received gasless transaction request: {:?}", payload);
+
+    // lets try to send fund this address with some gas
+
+    let iota_testnet = IotaClientBuilder::default().build_testnet().await.unwrap();
+
+    let address = payload.sender;
+
+    let request_from_faucet = utils::request_tokens_from_faucet(address, &iota_testnet).await.unwrap();
+
+    
+
+
+}
+
+
 
 
 
@@ -13,7 +47,12 @@ async fn main() -> Result<(), anyhow::Error> {
     // build our application with a single route
     let iota_testnet = IotaClientBuilder::default().build_testnet().await?;
     let msg = format!("welcome to IOTA Testnet {:?}", iota_testnet.api_version()) ;
-    let app = Router::new().route("/", get( move | | async { msg}));
+
+    //let app = Router::new().route("/", get( move | | async { msg}));
+
+    let app = Router::new()
+        .route("/", get(move || async { msg }))
+        .route("/gasless-transaction", post(handle_gasless_transaction));
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3001".parse().unwrap())
---
+++ backend/src/utils.rs
@@ -0,0 +1,114 @@
+
+use iota_sdk::{
+    IotaClient, IotaClientBuilder,
+    iota_client_config::{IotaClientConfig, IotaEnv},
+    rpc_types::IotaTransactionBlockResponseOptions,
+    types::{
+        base_types::{IotaAddress, ObjectID},
+        crypto::SignatureScheme::ED25519,
+        digests::TransactionDigest,
+        programmable_transaction_builder::ProgrammableTransactionBuilder,
+        quorum_driver_types::ExecuteTransactionRequestType,
+        transaction::{Argument, Command, Transaction, TransactionData},
+    },
+    wallet_context::WalletContext,
+};
+
+use iota_sdk::rpc_types::IotaObjectDataOptions;
+use anyhow::bail;
+use serde_json::json;
+use std::{str::FromStr, time::Duration};
+
+use reqwest::Client;
+
+pub const IOTA_FAUCET_BASE_URL: &str = "https://faucet.testnet.iota.cafe"; // testnet faucet
+
+#[derive(serde::Deserialize)]
+struct FaucetResponse {
+    task: String,
+    error: Option<String>,
+}
+
+
+
+
+/// Request tokens from the Faucet for the given address
+pub async fn request_tokens_from_faucet(
+    address: IotaAddress,
+    client: &IotaClient,
+) -> Result<(), anyhow::Error> {
+    let address_str = address.to_string();
+    let json_body = json![{
+        "FixedAmountRequest": {
+            "recipient": &address_str
+        }
+    }];
+
+    // make the request to the faucet JSON RPC API for coin
+    let reqwest_client = Client::new();
+    let resp = reqwest_client
+        .post(format!("{IOTA_FAUCET_BASE_URL}/v1/gas"))
+        .header("Content-Type", "application/json")
+        .json(&json_body)
+        .send()
+        .await?;
+    println!(
+        "Faucet request for address {address_str} has status: {}",
+        resp.status()
+    );
+    println!("Waiting for the faucet to complete the gas request...");
+    let faucet_resp: FaucetResponse = resp.json().await?;
+
+    let task_id = if let Some(err) = faucet_resp.error {
+        bail!("Faucet request was unsuccessful. Error is {err:?}")
+    } else {
+        faucet_resp.task
+    };
+
+    println!("Faucet request task id: {task_id}");
+
+    // wait for the faucet to finish the batch of token requests
+    let coin_id = loop {
+        let resp = reqwest_client
+            .get(format!("{IOTA_FAUCET_BASE_URL}/v1/status/{task_id}"))
+            .send()
+            .await?;
+        let text = resp.text().await?;
+        if text.contains("SUCCEEDED") {
+            let resp_json: serde_json::Value = serde_json::from_str(&text).unwrap();
+
+            break <&str>::clone(
+                &resp_json
+                    .pointer("/status/transferred_gas_objects/sent/0/id")
+                    .unwrap()
+                    .as_str()
+                    .unwrap(),
+            )
+            .to_string();
+        } else {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    };
+
+    // wait until the fullnode has the coin object, and check if it has the same
+    // owner
+    loop {
+        let owner = client
+            .read_api()
+            .get_object_with_options(
+                ObjectID::from_str(&coin_id)?,
+                IotaObjectDataOptions::new().with_owner(),
+            )
+            .await?;
+
+        if owner.owner().is_some() {
+            let owner_address = owner.owner().unwrap().get_owner_address()?;
+            if owner_address == address {
+                break;
+            }
+        } else {
+            tokio::time::sleep(Duration::from_secs(1)).await;
+        }
+    }
+    Ok(())
+}
